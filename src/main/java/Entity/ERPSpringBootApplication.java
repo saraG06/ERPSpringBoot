97df057db0782f5a7af302dc1742abb6f80e3988
saraGUnikey <sara.gaudiosi@unikey.it>
2022-05-04 15:05:44 +0200

    add src

---
+++ src/main/java/Entity/ERPSpringBootApplication.java
@@ -0,0 +1,14 @@
+package Entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERPSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERPSpringBootApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/it/unikey/autospring/AutoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unikey.autospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
