f2deaa9f0b269e2e94eba32d1ed1bf179f5be304
DESKTOP-GO6P2KJ\EmilianoUnikey <luca.papaleorm@gmail.com>
2022-05-06 17:56:57 +0200

    ciao

--- src/main/java/it/unikey/erpspringboot/PL/OperatoreControllerAssunzioneUltimoMese.java
+++ src/main/java/it/unikey/erpspringboot/PL/OperatoreControllerAssunzioneUltimoMese.java
@@ -2,14 +2,10 @@ package it.unikey.erpspringboot.PL;
 
 import it.unikey.erpspringboot.BLL.dto.Response.OperatoreResponseDTO;
 import it.unikey.erpspringboot.BLL.service.abstraction.OperatoreService;
-import it.unikey.erpspringboot.DAL.Entity.Operatore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,7 +19,7 @@ public class OperatoreControllerAssunzioneUltimoMese {
     private final OperatoreService operatoreService;
 
     @GetMapping(path = "/{now}")
-    public ResponseEntity<List<OperatoreResponseDTO>> getAllAssuntiUltimoMese(LocalDate now) {
+    public ResponseEntity<List<OperatoreResponseDTO>> getAllAssuntiUltimoMese(@PathVariable LocalDate now) {
         return new ResponseEntity<>(operatoreService.getAllAssuntiUltimoMese(now), HttpStatus.OK);
     }
 }
